667759a833d4eb5618fcfee4104d2b4b3d02c58e
parth-ed <parthm917@gmail.com>
2025-06-27 10:43:31 +0530

    Update messenger.sol

--- messenger.sol
+++ messenger.sol
@@ -1,23 +1,17 @@
 //SPDX-License-Identifier: MIT
- 
+
 pragma solidity 0.8.15;
- 
-contract TheBlockchainMessenger {
- 
-    uint public changeCounter;
- 
-    address public owner;
- 
-    string public theMessage;
- 
-    constructor() {
-        owner = msg.sender;
-    }
- 
-    function updateTheMessage(string memory _newMessage) public {
-        if(msg.sender == owner) {
-            theMessage = _newMessage;
-            changeCounter++;
-        }
-    }
+contract BlockschainMessenger {
+  uint public changeCounter;
+  address public owner;
+  string public theMessage;
+  constructor() {
+    owner = msg.sender;
+  }
+  function updateTheMessage(string memory _newMessage) public {
+     if(msg.sender == owner) {
+       theMessage = _newMessage;
+       change counter++;
+       }
+  }
 }
